c42c12a27761d6c73a6c477d783554ce7aa81cbb
Malte Franken <exxamalte@users.noreply.github.com>
2024-09-30 02:02:27 +1000

    Remove Python 3.8 support (#26)

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
     url=URL,
     packages=find_packages(exclude=("tests", "tests.*")),
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
