3fed432e225b20f30c0c4bfc2af08ffc96f457fe
Malte Franken <exxamalte@users.noreply.github.com>
2024-10-05 23:05:32 +1000

    Migrate to pyproject.toml (#28)
    * migrate to pyproject.toml
    
    * move version constant
    
    * fix typo
    
    * need to explicitly remove tests from source dist

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Test for the generic GeoJSON feed general setup."""
-
-from aio_geojson_generic_client import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
